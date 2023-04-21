91b1e07324716ecb01bdb126f40c02f40d87cf55
carlos danilo angarita <daniloangaritagarcia@gmail.com>
2023-04-21 12:07:31 -0500

    actualizacion dos

--- PROYECTOS/PROYECTO SIMULACION DE CORREOS/formulario.js
+++ PROYECTOS/PROYECTO SIMULACION DE CORREOS/formulario.js
@@ -1,19 +1,36 @@
 // proyecto de validacion de formulario
 
 document.addEventListener("DOMContentLoaded", () => {
+
+
+  const datos = {
+    email: "",
+    asunto: " ",
+    mensaje: ""
+  }
+
   // seleccionar elemetos del form
 
-  const email = document.getElementById("email"),
+    const email = document.getElementById("email"),
     asunto = document.getElementById("asunto"),
-    mensaje = document.getElementById("mensaje");
-  formulario = document.getElementById("formulario");
+    mensaje = document.getElementById("mensaje"),
+    formulario = document.getElementById("formulario"),
+    buttonSubmit = document.querySelector("#formulario button[type=submit]");
+    buttonSubmit = document.querySelector("#formulario button[type=reset]");
+
 
   //eventos del formulario
 
-  email.addEventListener("blur", validar);
-  asunto.addEventListener("blur", validar);
+  email.addEventListener("input", validar);
+  asunto.addEventListener("input", validar);
+  mensaje.addEventListener("input", validar);
 
-  mensaje.addEventListener("blur", validar);
+
+
+
+  //funciones principales
+
+
 
   function validar(e) {
     if (e.target.value.trim() === "") {
@@ -21,20 +38,33 @@ document.addEventListener("DOMContentLoaded", () => {
         `el campo ${e.target.id} es obligatorio`,
         e.target.parentElement
       );
+       // para habilitar o desabilitar nuevamnete el boton  de enviar si  se borran los campos 
+     datos[e.target.name] ="";
+      comprobarDatosObj()
       return;
-    } 
-   
-    if(e.target.id === "email" && !validarEmail(e.target.value)){
-      mostrarAlerta("email no es valido", e.target.parentElement)
-      return;
-   }
-    
-   ElimanrAlerta( e.target.parentElement)
+    }
 
+    if (e.target.id === "email" && !validarEmail(e.target.value)) {
+      mostrarAlerta("email no es valido", e.target.parentElement) 
+    // para habilitar o desabilitar nuevamnete el boton  de enviar si  se borran los campos 
+      datos[e.target.name] ="";
+      comprobarDatosObj()
+      return;
+    }
+
+    ElimanrAlerta(e.target.parentElement)
+    // alacenar datos  desde el formulario en el objeto datos
+
+    datos[e.target.name] = e.target.value.trim().toLowerCase();
+    // COMPROBAR DATOS
+
+    comprobarDatosObj()
   }
 
+
+
   // funciones auxiliares
-  
+
   /**
    *
    * @param {mensaje de alerta} mensaje
@@ -42,7 +72,7 @@ document.addEventListener("DOMContentLoaded", () => {
    */
   function mostrarAlerta(mensaje, referencia) {
     console.log("hubo un error ");
-    
+
     ElimanrAlerta(referencia)
     /*let existealerta = referencia.querySelector(".bg-red-600");
     if (existealerta) {
@@ -56,23 +86,37 @@ document.addEventListener("DOMContentLoaded", () => {
     // incrustar error al formulario
     referencia.appendChild(alerta);
   }
+
+
+  function ElimanrAlerta(referencia) {
+    // elimina la alerta si ya fue copiada
+    let existealerta = referencia.querySelector(".bg-red-600");
+    if (existealerta) {
+      existealerta.remove();
+    }
+
+
+  }
+
+  function validarEmail(email) {
+    //validacion si el email esta correctamente
+    const regex = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/
+    resultado = regex.test(email);
+    console.log(resultado);
+    return resultado;
+  }
+
+
+  function comprobarDatosObj() {
+    // comprueba si el formulario recibio datos
+    if (Object.values(datos).includes("")) {
+      buttonSubmit.classList.add("opacity-50")
+      buttonSubmit.disabled = true
+      return;
+    } 
+      buttonSubmit.classList.remove("opacity-50")
+      buttonSubmit.disabled = false
+  
+  }
+
 }); 
-
-
-function ElimanrAlerta(referencia){
-   // elimina la alerta si ya fue copiada
-   let existealerta = referencia.querySelector(".bg-red-600");
-   if (existealerta) {
-     existealerta.remove();
-   }
-
-
-}
- 
-function validarEmail(email) {
-//validacion si el email esta correctamente
-  const regex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ 
-  resultado=regex.test(email);
-  console.log(resultado);
-  return resultado;
-}
